07baaf31fbf0f25a3f5f7f5902e097963d1bcd93
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2024-02-25 15:53:27 +0700

    finished bar-chart

--- src/components/Charts/BarChart.jsx
+++ src/components/Charts/BarChart.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-const BarChart = () => {
-  return <div>BarChart</div>;
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+
+const BarCharts = () => {
+  return (
+    <BarChart
+      className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl"
+      data={barCustomSeries[0].dataSource}
+      width={500}
+      height={300}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={barCustomSeries[0].xName} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      {barCustomSeries.map((series, index) => (
+        <Bar
+          key={index}
+          dataKey={series.yName}
+          name={series.name}
+          fill={series.fill}
+        />
+      ))}
+    </BarChart>
+  );
 };
 
-export default BarChart;
+export default BarCharts;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -17,4 +17,4 @@ export { default as LineChart } from "./Charts/LineChart";
 export { default as Stacked } from "./Charts/Stacked";
 export { default as Pie } from "./Charts/Pie";
 
-export { default as BarChart } from "./Charts/BarChart";
+export { default as BarCharts } from "./Charts/BarChart";
